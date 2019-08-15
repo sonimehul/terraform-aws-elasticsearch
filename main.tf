f1fba31a1cf78d17bee06732163023cddc0061df
sonimehul <soni.mehul@octoconsulting.com>
2019-08-15 12:19:38 -0400

    Added custom suffix to aws_iam_service_linked_role

--- main.tf
+++ main.tf
@@ -66,6 +66,7 @@ resource "aws_iam_service_linked_role" "default" {
   count            = var.enabled && var.create_iam_service_linked_role ? 1 : 0
   aws_service_name = "es.amazonaws.com"
   description      = "AWSServiceRoleForAmazonElasticsearchService Service-Linked Role"
+  custom_suffix    = "TF"
 }
 
 # Role that pods can assume for access to elasticsearch and kibana
